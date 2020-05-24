828c7675b73d5ccb550eaa95f602277001d6cf29
Arjun143aish <59442901+Arjun143aish@users.noreply.github.com>
2020-05-24 16:50:57 +0530

    Delete request.py

--- request.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'PassengerId':892, 'Pclass':3, 'Age':34.5,'SibSp':0,
-                            'Fare':7.8292,'Sex_male':1,'Embarked_Q':1,'Embarked_S':0})
-
-print(r.json())
-    
